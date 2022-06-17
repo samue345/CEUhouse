06661eed00f92b199a0d0e4c8f7cd1e2c267bfa0
Samuel Souza <85644942+samue345@users.noreply.github.com>
2022-06-16 23:48:14 -0300

    ultima atualização do dia

--- ProjetoSW/SW.js
+++ ProjetoSW/SW.js
@@ -7,7 +7,15 @@ window.open("login.html", "_self")
    
 }
 
+function per(){
+    perfil.style.background= "rgba(000, 000, 000, 0.6)"
+}
+function pern(){
+    perfil.style.background="";
+}
 
 perfil.addEventListener("click", perfill);
+perfil.addEventListener("mouseover", per);
+perfil.addEventListener("mouseleave", pern);
 
 }
