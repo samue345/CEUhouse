27469d09cfd4a7b0e6e0f9c4e49911d0d0689aef
Samuel Souza <85644942+samue345@users.noreply.github.com>
2022-09-06 14:42:34 -0300

    commit terça feira 2:42

--- ProjetoSW/minharep.php
+++ ProjetoSW/minharep.php
@@ -1,12 +1,12 @@
 <?
-    include_once "conexao.php";
-    include_once "imoveis.model.php";
-    include_once "imoveis.service.php";
-$imoveis= new Imoveis();
-$id2=$_GET['id'];
-$conexao= new Conexao();
+include_once "conexao.php";
+include_once "imoveis.model.php";
+include_once "imoveis.service.php";
+$imoveis = new Imoveis();
+$id2 = $_GET['id'];
+$conexao = new Conexao();
 $imoveisService = new imoveisService($conexao, $imoveis);
-$tarefas=$imoveisService->recuperar2($id2);
+$tarefas = $imoveisService->recuperar2($id2);
 
 
 ?>
@@ -14,118 +14,154 @@ $tarefas=$imoveisService->recuperar2($id2);
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Pagina inicial</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina inicial</title>
 
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/estilo.css">
-  <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="fontawesome/css/all.min.css">
 
 </head>
 
 <body>
-  <header>
-    <nav class="navbar navbar-expand-lg navbar-light  navegacao-1">
-      <a class="navbar-brand" href="index.php">
-        <img src="images/logo.png" class="logo">
-      </a>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light  navegacao-1">
+            <a class="navbar-brand" href="index.php">
+                <img src="images/logo.png" class="logo">
+            </a>
 
 
-      <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
-        <span class="navbar-toggler-icon"></span>
-      </button>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#nav-target">
+                <span class="navbar-toggler-icon"></span>
+            </button>
 
-      <div class="collapse navbar-collapse" id="nav-target">
+            <div class="collapse navbar-collapse" id="nav-target">
 
-        <form class="flex">
-          <input type="text" placeholder="Universidade - Campus" class="meu form-control">
-          <button class="button-principal"><i class="fa-solid fa-magnifying-glass"></i></button>
-        </form>
+                <form class="flex">
+                    <input type="text" placeholder="Universidade - Campus" class="meu form-control">
+                    <button class="button-principal"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
 
-        <ul class="navbar-nav ml-auto nave-principal">
+                <ul class="navbar-nav ml-auto nave-principal">
 
-          <li class="nav-item li-pri" id="per">
-            <i class="fa-solid fa-user perfil" id="perfil"></i>
-            <div class="caixa-login-some" id="login">
-              <ul>
-                <li>
-                  <a href="login.html" target="_self" class="entrar">entrar</a>
-                </li>
-                <hr>
-                <li>
-                  <a href="cadastrar.html" class="cadastre">Cadastre-se</a>
-                </li>
-              </ul>
+                    <li class="nav-item li-pri" id="per">
+                        <i class="fa-solid fa-user perfil" id="perfil"></i>
+                        <div class="caixa-login-some" id="login">
+                            <ul>
+                                <li>
+                                    <a href="login.html" target="_self" class="entrar">entrar</a>
+                                </li>
+                                <hr>
+                                <li>
+                                    <a href="cadastrar.html" class="cadastre">Cadastre-se</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+
+                </ul>
+
             </div>
-          </li>
+        </nav>
 
+    </header>
+    <nav class=" navegacao">
+        <ul class="flex ul-2">
+            <li class="lista-2">
+                <a href="" class="link-li2">sobre</a>
+            </li>
+            <li class="lista-2">
+                <a href="" class="link-li2">favoritos</a>
+            </li>
+            <li class="lista-2">
+                <a href="" class="link-li2">Contato</a>
+            </li>
+            <li class="lista-2">
+                <a href="" class="link-li2">Historia</a>
+            </li>
+            <li class="lista-2">
+                <a href="" class="link-li2">Melhores avaliações</a>
+            </li>
         </ul>
 
-      </div>
     </nav>
+    <h5>Republica:</h5>
+    <div class="cont bg-warning">
+        <div class="row justify-content-center mb-2">
+        <div class="col-lg-10">
+            <div id="demo" class="carousel slide" data-ride="carousel">
 
-  </header>
-  <nav class=" navegacao">
-    <ul class="flex ul-2">
-      <li class="lista-2" id="filtro"><i class="fa-solid fa-bars"></i> filtros </li>
-      <li class="lista-2">
-        <a href="" class="link-li2">sobre</a>
-      </li>
-      <li class="lista-2">
-        <a href="" class="link-li2">favoritos</a>
-      </li>
-      <li class="lista-2">
-        <a href="" class="link-li2">Contato</a>
-      </li>
-      <li class="lista-2">
-        <a href="" class="link-li2">Historia</a>
-      </li>
-      <li class="lista-2">
-        <a href="" class="link-li2">Melhores avaliações</a>
-      </li>
-    </ul>
+                <!-- Indicators -->
+                <ul class="carousel-indicators">
+                    <? $i = 0;
+                    foreach ($tarefas as $row) {
+                        $actives = '';
+                        if ($i == 0) {
+                            $actives = 'active';
+                        }
+                    ?>
+                        <li data-target="#demo" data-slide-to="<?= $i ?>" class="<?= $actives ?>"></li>
 
-  </nav>
- <!-- 
-  <div class="grid_d">
-    <? /*foreach ($tarefas as $key => $tarefa) { ?>
-      <?php $id = $tarefa->id_matricula; ?>
+                </ul>
+            <? $i++;
+                    } ?>
 
-      <div class="index_card" id="imovel_<?= $tarefa->id_matricula ?>" onclick="location.href='minharep.php?id=<?= $tarefa->id_matricula ?>'">
-        <section class="seim">
-          <img src="<?= $tarefa->fotos ?>" alt="" width="276" height="200" class="imgf">
+            <!-- The slideshow -->
+            <div class="carousel-inner">
+                <?php
+                $i = 0;
+                foreach ($tarefas as $row) {
+                    $actives = '';
+                    if ($i == 0) {
+                        $actives = 'active';
+                    }
+                ?>
+                    <div class="carousel-item <?= $actives; ?>">
+                        <img src="<?= $row->imagens ?>" width="400" height="400">
+                    </div>
+                    <? $i++;} ?>
 
-        </section>
+            </div>
 
-        <section class=" info">
+                    <!-- Left and right controls -->
+                    <a class="carousel-control-prev" href="#demo" data-slide="prev">
+                        <span class="carousel-control-prev-icon"></span>
+                    </a>
+                    <a class="carousel-control-next" href="#demo" data-slide="next">
+                        <span class="carousel-control-next-icon"></span>
+                    </a>
 
-          <ul>
-            <li class="lista-card">nome do anfitrião: <?= $tarefa->nome_anfitriao ?></li>
-            <li class="lista-card">bloco: <?= $tarefa->bloco ?></li>
-            <li class="lista-card">apartamento: <?= $tarefa->apartamento ?></li>
-            <li class="lista-card">numero de pessoas: <?= $tarefa->numero_de_pessoas ?></li>
-          </ul>
+            </div>
+            </div>
+        </div>
+    </div>
+    <section>
+        <?$id=0;
+        foreach($tarefas as $key => $row){?>
+            <?if($row->id_matricula != $id){?>
+                <ul>
+                   <li>nome do anfitrião: <?=$row->nome_anfitriao?></li>
+                    <li>apartamento: <?=$row->apartamento?></li>
+                    <li>bloco: <?=$row->bloco?></li>
+                    <li>numero de pessoas da Republica: <?=$row->numero_de_pessoas?></li>
+                    <li>Republica:<?=$row->sexo?></li>
+
+                </ul>
+                <?} $id=$row->id_matricula; ?>
+       <? }?>
+    </section>
 
 
-        </section>
-
-      </div>
-
-    <? } */?>
-  </div>
-  !-->
 
 
 
 
-
-
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  <script src="SW.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script src="SW.js"></script>
 </body>
 
 </html>
