5d4ed99d5b0808eb8a0f59449d4c856437c65602
Samuel Souza <85644942+samue345@users.noreply.github.com>
2022-09-16 23:51:36 -0300

    aaa

--- ProjetoSW/foto.php
+++ ProjetoSW/foto.php
@@ -66,6 +66,7 @@
       </section>
       <?} ?>
     
+    <h4 class="h4_foto">Envie mais fotos do quarto ou apartamento aaa</h4>
     <div class="div-foto">
     <form action="index_controller.php?acao=1"  enctype="multipart/form-data" method="post"  class="formulario_foto">
       <section class="fotoin">
